a14a90c896326f6bd6b77d501690afe4bd4ed6a2
stintosestudios <stintose.studios@gmail.com>
2017-01-26 14:36:12 -0500

    project update

--- projects/piston/scene.js
+++ projects/piston/scene.js
@@ -31,7 +31,7 @@ scene({
             id : 'piston_wheel',
             w : 200,
             h : 200,
-            x : 100,
+            x : 25,
             y : 100,
 
             forFrame : function (pt) {},
@@ -72,9 +72,13 @@ scene({
                 radian = Math.PI * 2 * this.percentDone,
                 cx = ptWheel.x + ptWheel.w / 2 - pt.w / 2,
                 cy = ptWheel.y + ptWheel.h / 2 - pt.h / 2;
-				
-				//pt.radian = radian;
-				
+
+                //  additional data that is to be shared with other parts
+                pt.local = {
+
+                    radian : radian
+
+                };
 
                 pt.x = cx + Math.cos(radian) * 25;
                 pt.y = cy + Math.sin(radian) * 25;
@@ -103,7 +107,79 @@ scene({
 
             }
 
-        },
+        }, {
+
+            id : 'piston_shaft',
+
+            w : 64,
+            h : 16,
+            x : 400,
+            y : 200,
+
+            skin : {
+
+                appendRender : function (ctx, skin) {
+
+                    var pt = skin.part;
+
+                    // the part area
+                    ctx.strokeStyle = '#ff0000';
+                    ctx.lineWidth = 1;
+                    ctx.strokeRect(0, 0, pt.w, pt.h);
+
+                    ctx.strokeStyle = '#00ffff';
+                    ctx.lineWidth = 3;
+
+                    ctx.strokeRect(0, 0, pt.w, pt.h);
+
+                }
+
+            }
+        }, {
+
+            id : 'piston_bar',
+
+            w : 300,
+            h : 16,
+
+            forFrame : function (pt) {
+
+                // wheel link
+                var ptWL = this.parts['piston_wheel_link'],
+                ptSH = this.parts['piston_shaft'],
+                ptWheel = this.parts['piston_wheel'],
+                //cx = ptWL.x + ptWL.w / 2 - pt.h / 2,
+                //cy = ptWL.y + ptWL.h / 2 - pt.h / 2;
+                cx = ptWL.x + ptWL.w / 2,
+                cy = ptWL.y + ptWL.h / 2;
+
+                pt.radian = Math.atan2(cy - (ptSH.y+ptSH.h/2), cx - (ptSH.x + ptSH.w/2));
+                pt.x = cx;
+                pt.y = cy;
+
+            },
+
+            skin : {
+
+                appendRender : function (ctx, skin) {
+
+                    var pt = skin.part;
+
+                    // the part area
+                    ctx.strokeStyle = '#ff0000';
+                    ctx.lineWidth = 1;
+                    ctx.strokeRect(0, 0, pt.w, pt.h);
+
+                    ctx.strokeStyle = '#00ffff';
+                    ctx.lineWidth = 3;
+
+                    ctx.strokeRect(0, 0, pt.w, pt.h);
+
+                }
+
+            }
+
+        }
 
     ],
 
